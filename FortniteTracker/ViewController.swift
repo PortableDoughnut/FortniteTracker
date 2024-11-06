3063a066048d03d54f87c32445191eb15424f7ef
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-06 08:39:18 -0700

    Initial Commit

---
+++ FortniteTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortniteTracker
+//
+//  Created by Gwen Thelin on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
